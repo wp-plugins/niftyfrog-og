8cdea3171259ccae8d13fb021b20d601622d8913
misschell <misschell@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-03 22:29:32 +0000

    Creating tag for version 0.1
    git-svn-id: https://plugins.svn.wordpress.org/niftyfrog-og/tags/0.1@1104493 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ niftyfrogog.php
@@ -0,0 +1,225 @@
+<?php
+/*
+	Plugin Name: NiftyFrog OG
+	Plugin URI: http://niftyfrog.com/
+	Description: Places Open Graph and Twitter Card meta tags in your blog's header, so a suitable image and description show, when crossposting to Facebook or Twitter.
+	Version: 0.1
+	Author: Michelle Thompson
+	Author URI: http://mthompsondev.com/
+	License: GPLv3
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// Add admin menu item
+function nf_og_menu_item() {
+	$thispage = add_options_page( 'NiftyFrog OG Options', 'NF OG Options', 'manage_options', 'nf-og-options', 'nf_og_setup_screen' );
+	add_action( 'admin_print_styles-' . $thispage, 'admin_nf_styles' );
+	add_action( 'admin_print_scripts-' . $thispage, 'admin_nf_scripts' );
+}
+
+// Enable options, and register css and js    
+function nf_og_options_init(){
+	register_setting( 'niftyfrogog', 'nfogoptions', 'check_input' );
+	wp_register_style( 'nfadminstyles', plugins_url('styles.css', __FILE__) );
+	wp_register_script( 'nfadminjs', plugins_url('nfadmin.js', __FILE__) );
+}
+
+// Delete options from DB
+function nf_og_options_del(){
+	delete_option( 'nfogoptions' );
+}
+
+// Disable options
+function nf_og_options_undo(){
+	unregister_setting( 'niftyfrogog', 'nfogoptions', 'check_input' );
+	wp_dequeue_style( 'nfadminstyles' );
+	wp_dequeue_script( 'nfadminjs' );
+	wp_deregister_style( 'nfadminstyles' );
+	wp_deregister_script( 'nfadminjs' );
+}
+
+// Sanitize user form input
+function check_input( $input ){
+	// Strip tags, and make sure image url starts correctly
+	$this_def_img = trim( strip_tags( $input['defaultimg'] ) );
+	if ( preg_match( '~((http|https)://)(.+?)~', $this_def_img ) ):
+		$input['defaultimg'] = $this_def_img;
+	else:
+		$input['defaultimg'] = 'no'; // Signal not to add as a meta tag in page output;
+	endif;
+	
+	// Strip out all but numbers and commas from FB ID
+	$this_fb_userid = preg_replace( array( '/[^\d,]/', '/(?<=,),+/', '/^,+/', '/,+$/' ), '', $input['fbuserid'] );
+	$this_fb_userid = trim( $this_fb_userid );
+	if ( $this_fb_userid === '' ):
+		$input['fbuserid'] = 'no'; // Signal not to add as a meta tag in page output
+	else:
+		$input['fbuserid'] = $this_fb_userid;		
+	endif;
+	
+	// Strip out all but letters, numbers, and underscores from Twitter name
+	$this_twit_userid = str_ireplace( 'Twitter user ID not set', '', $input['twuserid'] );
+	$this_twit_userid = preg_replace( '/[^\w\d_]+/', '', $this_twit_userid );
+	if ( $this_twit_userid === '' ):
+		$input['twuserid'] = 'no'; // Signal not to add as a meta tag in page output
+	else:
+		$input['twuserid'] = $this_twit_userid;		
+	endif;
+	
+	return $input;
+}
+
+// Print the admin screen 
+function nf_og_setup_screen() {
+	print '<div class="nfadmincontainer">';
+	print '<h3>NiftyFrog OG Options</h3>';	
+	print '<form method="post" action="options.php">';
+	settings_fields('niftyfrogog');
+	$options = get_option('nfogoptions');
+		
+	$fb_user_id = 'Facebook user ID not set';
+	if ( $options['fbuserid'] && $options['fbuserid'] !=='' && $options['fbuserid'] !== 'no' ):
+		$fb_user_id = $options['fbuserid'];
+	endif;
+	
+	$twit_user_id = 'Twitter user ID not set';
+	if ( $options['twuserid'] && $options['twuserid'] !=='' && $options['twuserid'] !== 'no' ):
+		$twit_user_id = $options['twuserid'];
+	endif;
+	
+	$current_img = 'Default image not set';
+	$img_view_line = '';	
+	if ( $options['defaultimg'] && $options['defaultimg'] !== '' && $options['defaultimg'] !== 'no' ):
+		$current_img = $options['defaultimg'];
+		$img_view_line = '<p>Current default image:<br /><img src="' . $current_img . '" title="Current Image" class="defaultimg" /></p>';
+	endif;	
+	
+	
+	print '<p>Enter your Facebook <em>numeric</em> user ID here. Facebook uses this information to determine who has access to page insights, and who can administer Facebook apps for your page. You can enter multiple ID&#39;s, separated by commas. <span id="findfbid">You can find your numeric Facebook ID by visiting http://graph.facebook.com/&lt;Your Username&gt;.</span></p>';
+	print '<div class="nfogrow"><div class="nfoglabel">Your numeric Facebook ID:</div>';
+	print '<input type="text" name="nfogoptions[fbuserid]" value="' . $fb_user_id . '" size="33" onClick="this.setSelectionRange(0, this.value.length)" /></div>';	
+	print '<p>Enter your Twitter user ID here, without the "&#64;" sign. Twitter uses this information for card analytics.</p>';
+	print '<div class="nfogrow"><div class="nfoglabel">Your Twitter ID:</div>';
+	print '<input type="text" name="nfogoptions[twuserid]" value="' . $twit_user_id . '" size="33" onClick="this.setSelectionRange(0, this.value.length)" /></div>';	
+	print '<p>The default image you specify here will be used in the meta tags that are created, if no featured image is set for, and no other image is found in, a post or page. Enter the full URL, beginning with http://, to your default image. Make sure the image size is at least 200px by 200px, so that Facebook will recognize it.</p>'; 
+	print '<div class="nfogrow"><div class="nfoglabel">Full URL to your default image:</div>';
+	print '<input type="text" name="nfogoptions[defaultimg]" value="' . $current_img . '" size="33" onClick="this.setSelectionRange(0, this.value.length)" /></div>';	
+	print '<div class="nfogrow"><input type="submit" value="Submit" /></div>';
+	print '</form>';
+	print $img_view_line;
+	print '<p>There are no other options to set. This plugin uses the information from your individual posts to create the necessary meta tags.</p>';
+	print '</div>';
+	print_donate_button();
+}
+
+// Print meta tags for header
+function print_meta_tags() {
+	$options = get_option('nfogoptions');
+	$nfog_fb_userid = 'no';
+	if ( $options['fbuserid'] && $options['fbuserid'] !== '' ):
+		$nfog_fb_userid = $options['fbuserid'];
+	endif;	
+	$nfog_tw_userid = 'no';
+	if ( $options['twuserid'] && $options['twuserid'] !== '' ):
+		$nfog_tw_userid = $options['twuserid'];
+	endif;		
+	$nfog_title = get_bloginfo('name');
+	$nfog_type = 'website';
+	$nfog_url = get_bloginfo('url');
+	$nfog_descr = get_bloginfo('description');
+	$nfog_img = 'no';	
+	if ( $options['defaultimg'] && $options['defaultimg'] !== '' ):
+		$nfog_img = $options['defaultimg'];
+	endif;
+	$no_img_msg = '<!-- No Default Image set -->';
+	$no_fb_userid_msg = '<!-- No Facebook Admin ID set -->';
+	$no_tw_userid_msg = '<!-- No Twitter ID set -->';
+	if(is_single() || is_page()):
+		$cross_post_id = $post->ID; // Get the post ID
+		$nfog_title = get_the_title( $cross_post_id );
+		$nfog_type = 'article';
+		$nfog_url = get_permalink( $cross_post_id );
+		
+		// Get post content
+		$thisPost = get_post( $cross_post_id );
+		$cpcontent = $thisPost->post_content;
+		
+		// Check for images, and retrieve the featured, first, or default image as the og image
+		if( has_post_thumbnail( $cross_post_id ) ):
+			$featured_img_id = get_post_thumbnail_id( $cross_post_id );
+			$nfog_img = wp_get_attachment_url( $featured_img_id );
+		else:	
+			if( strpos( $cpcontent, "<img" ) ):
+				$first_img = '';
+				$output = preg_match_all('/<img.+src=[\'"]([^\'"]+)[\'"].*>/i', $cpcontent, $matches);
+				$nfog_img = $matches [1] [0];
+			endif;
+		endif;
+		
+		// Get post excerpt, or use the content, and tidy it
+		if ( $thisPost->post_excerpt ):
+		$cpcontent = $thisPost->post_excerpt;
+		endif;
+		$nfog_descr = strip_tags( strip_shortcodes( $cpcontent ) );
+		$nfog_descr = str_replace( array( "\n", "\t", "\r" ), '', $nfog_descr );
+		// Shorten and add ... if more than 200 chars
+		if ( strlen( utf8_decode( $nfog_descr ) ) > 200 ):
+			$word_cutoff = strrpos( substr( $nfog_descr, 0, 197 ), ' ' );
+			$nfog_descr = trim( substr( $nfog_descr, 0, $word_cutoff ) ) . '...';
+		endif;	
+	endif;
+	
+	// Print the meta tags
+	print "\n<!-- NiftyFrog OG for Facebook and Twitter Crossposting -->\n";	
+	if ( $nfog_tw_userid === 'no' ):
+		print $no_tw_userid_msg . "\n";		
+	else:
+		print '<meta name="twitter:card" content="summary"' . " />\n";
+		print '<meta property="twitter:site" content="&#64;' . $nfog_tw_userid . "\" />\n";
+	endif;
+	if ( $nfog_fb_userid === 'no' ):
+		print $no_fb_userid_msg . "\n";		
+	else:		
+		print '<meta property="fb:admins" content="' . $nfog_fb_userid . "\" />\n";
+	endif;
+	print '<meta property="og:site_name" content="' . get_bloginfo('name') . "\" />\n";
+	print '<meta property="og:title" content="' . $nfog_title . "\" />\n";
+	print '<meta property="og:type" content="' . $nfog_type . "\" />\n";
+	print '<meta property="og:url" content="' . $nfog_url . "\" />\n";
+	print '<meta property="og:description" content="' . $nfog_descr . "\" />\n";
+	if ( $nfog_img === 'no' ):
+		print $no_img_msg . "\n";
+	else:
+		print '<meta property="og:image" content="' . $nfog_img . "\" />\n";
+	endif;
+	print "<!-- End NiftyFrog OG for Facebook and Twitter Crossposting -->\n\n";
+}
+
+// enqueue scripts
+function admin_nf_scripts() {
+	wp_enqueue_script( 'nfadminjs' );
+}
+
+// enqueue styles
+function admin_nf_styles() {
+	wp_enqueue_style( 'nfadminstyles' );
+}
+
+function print_donate_button() {
+	print '<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">';
+	print '<div class="nfogrow"><div class="nfoglabel regfontwt">My coding is fueled by coffee. If you find this plugin helpful, feel free to buy me a cup of my favorite brew.</div> ';
+	print '<input type="hidden" name="cmd" value="_s-xclick">';
+	print '<input type="hidden" name="hosted_button_id" value="CZA4ZWJUVVZHN">';
+	print '<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"></div>';
+	print '<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">';
+	print '</form>';
+}
+
+add_action('admin_init', 'nf_og_options_init' );
+add_action( 'admin_menu', 'nf_og_menu_item' );
+add_action( 'wp_head', 'print_meta_tags' );
+register_deactivation_hook( __FILE__, 'nf_og_options_undo' );
+register_uninstall_hook( __FILE__, 'nf_og_options_del' );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,62 @@
+=== NiftyFrog OG ===
+Contributors: misschell
+Tags: og, Twitter, Twitter Card, meta tags, Facebook, crossposting, Open Graph, crosspost, og:image
+Donate link: http://niftyfrog.com
+Requires at least: 3.4
+Tested up to: 4.1.1
+Stable tag: 0.1
+License: GPLv3
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+
+Places meta tags in your blog's header, so a suitable image and description show, when crossposting to Facebook or generating a Twitter Card.
+
+== Description ==
+Have you ever tried to crosspost from your blog to Facebook, only to find that Facebook stubbornly refuses to share the post with a relevant image? Or that the description is either missing, or from some other section of your page? This plugin places meta tags for Facebook and Twitter Cards in your blog's header, so that these services don't have to guess what to show as your crosspost.
+
+Instead of a random button from your sidebar being shown as the thumbnail, the featured image from your post is specified. If this image hasn't been set, the first image in your post will be specified. If there is no image in your post, a default image, which you set in the plugin options, will be specified.
+
+If you have written an excerpt for your post, the crosspost text is taken from this. If you haven't, it is taken from the post content.
+
+If you set your Facebook user ID in the plugin options, a meta tag will be created that lets Facebook know you are the admin for any Facebook apps/widgets/insights for the page.
+
+= Note =
+This plugin has been written for, and tested on, single site installs, not multi-site.
+
+== Installation ==
+1. Unzip the zip file, and upload the entire folder to your plugins directory (/wp-content/plugins/).
+2. Activate the NiftyFrog OG plugin in your WordPress dashboard.
+3. In the dashboard, go to Settings -> NF OG Options, and enter your Facebook numeric user id, Twitter ID, and the URL to your default image.
+
+== Frequently Asked Questions ==
+= What if I don't enter my Facebook user ID? =
+
+The user ID is used to create a fb:admins meta tag, which Facebook uses to determine who can do things like view insight information for your page. This tag will not be created until/unless you set the user ID. Other meta tags will still be created.
+
+= What if I don't set a default image URL? =
+
+If you don't set this URL, this plugin will point the og:image and twitter:image meta tags to an image in the content of your post or page, if one exists. If it does not, these meta tags will not be created.
+
+= Will NiftyFrog OG work on a WordPress multisite network? =
+
+This plugin has been developed for a single blog site, not a multisite setup.
+
+= After installing this, why doesn't my crosspost to Facebook look different than it did before? =
+
+If you have already posted a link to Facebook, Facebook has stored that meta information, and will continue to display links to it the same way. Refresh this information on Facebook, at https://developers.facebook.com/tools/debug/og/object/
+
+= How long will my post description be? =
+
+The description that will be shown when crossposting is limited to 200 characters, but may be shortened to cut off after a word, rather than in the middle of a word.
+
+= Why isn't a Twitter Card created when I crosspost to Twitter? =
+
+Before cards will be created for links to your pages, you will have to request approval for your website from Twitter, at https://cards-dev.twitter.com/validator
+
+== Screenshots ==
+1. Select NF OG Options in the Settings menu.
+2. Only three options need to be set up.
+3. Helper link or popup for finding numeric Facebook ID.
+
+== Changelog ==
+= 0.1 =
+* Initial version
